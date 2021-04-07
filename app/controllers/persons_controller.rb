a8db79189f6427330936928c79e31b492ac8d4d6
vova <lililolo1222@mail.ru>
2021-04-07 19:23:08 +0300

    add profiles

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -2,6 +2,9 @@ class PersonsController < ApplicationController
   before_action :authenticate_user!
   def profile
   end
+  def index
+    @persons = User.all
+  end
   def show
     @person = User.find(params[:id])
     @person_posts = @person.posts
